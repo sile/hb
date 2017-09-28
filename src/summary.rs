4ae14905dea6a8a22f957fb4e00a0927821c28f0
Takeru Ohta <phjgt308@gmail.com>
2017-09-29 00:56:25 +0900

    Add unbiased variance and standard deviation

--- src/summary.rs
+++ src/summary.rs
@@ -54,6 +54,8 @@ pub struct Latency {
     pub median: Seconds,
     pub mean: Seconds,
     pub max: Seconds,
+    pub var: f64,
+    pub sd: f64,
 }
 impl Latency {
     fn new(results: &[RequestResult]) -> Self {
@@ -62,11 +64,45 @@ impl Latency {
         }
         let mut times = results.iter().map(|r| r.elapsed()).collect::<Vec<_>>();
         times.sort();
+
+        let var = unbiased_variance(&times);
         Latency {
             min: times[0],
             median: times[times.len() / 2],
             mean: Seconds(times.iter().map(|t| t.0).sum::<f64>() / times.len() as f64),
             max: *times.last().unwrap(),
+            var,
+            sd: var.sqrt(),
         }
+    }
+}
+
+fn unbiased_variance(samples: &[Seconds]) -> f64 {
+    if samples.len() < 2 {
+        return 0.0;
+    }
+
+    let n = samples.len() as f64;
+    let (sqsum, sum) = samples.iter().fold(
+        (0.0, 0.0),
+        |(a, b), s| (a + s.0 * s.0, b + s.0),
+    );
+    let avg = sum / n;
+    (sqsum - n * avg * avg) / (n - 1.0)
+}
+
+#[cfg(test)]
+mod test {
+    use run::Seconds;
+    use super::*;
+
+    #[test]
+    fn var_works() {
+        let samples = [0.7, -1.6, -0.2, -1.2, -0.1, 3.4, 3.7, 0.8, 0.0, 2.0]
+            .iter()
+            .cloned()
+            .map(Seconds)
+            .collect::<Vec<_>>();
+        assert_eq!((unbiased_variance(&samples) * 10000.0).floor(), 32005.0);
     }
 }
